27d014ed608697bd26ce9ce84481841e402656f8
siagianmiransyagresia-lab <siagianmiransyagresia@gmail.com>
2025-10-01 21:27:02 +0700

    pair 13
    selesai

---
+++ T00.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.Math;
+
+public class T00 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, hasil;
+
+        a = Integer.parseInt(input.nextLine());
+        b = Integer.parseInt(input.nextLine());
+        if (a % 2 == 0) {
+            System.out.println("Bilangan pertama: Genap");
+        } else {
+            System.out.println("Bilangan pertama: Ganjil");
+        }
+        if (b % 2 == 0) {
+            System.out.println("Bilangan kedua: Genap");
+        } else {
+            System.out.println("Bilangan kedua: Ganjil");
+        }
+        if (a > b) {
+            System.out.println("Bilangan pertama lebih besar");
+        } else {
+            if (a < b) {
+                System.out.println("Bilangan kedua lebih besar");
+            } else {
+                System.out.println("kedua bilangan sama besar");
+            }
+        }
+        if (a % 2 == 0 && b % 2 == 0) {
+            hasil = a + b;
+            System.out.println("hasil penjumlahan: " + hasil);
+        } else {
+            if (a % 2 != 0 && b % 2 != 0) {
+                hasil = a * b;
+                System.out.println("Hasil perkalian: " + hasil);
+            } else {
+                System.out.println("berbeda jenis");
+            }
+        }
+    }
+}
